c1af36685b8949f8407f3f42a2b755755aa527cc
nswierzbinowicz <161207554+nswierzbinowicz@users.noreply.github.com>
2025-03-01 12:51:44 +0100

    Add files via upload

---
+++ Zajecia1/main.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+//    Zad1.2.1
+//    printf("Hello world!\n");
+//    Zad1.2.2
+//    printf("Bardzo\ndlugi\nnapis");
+//    Zad1.2.3
+//    printf("Napis zawieraj¹cy rozne dziwne znaki // \\ \\ $ & %%");
+//    Zad1.2.4
+//    printf("Podaj liczbe\n");
+//    int a;
+//    scanf("%d",&a);
+//    printf("%d",a);
+//    Zad1.2.5
+//    printf("Podaj liczbê wymiern¹\n");
+//    float b;
+//    scanf("%f",&b);
+//    printf("%f",b);
+//    Zad1.2.6
+//    printf("Podaj 3 liczby\n");
+//    int a;
+//    int b;
+//    int c;
+//    scanf("%d",&a);
+//    scanf("%d",&b);
+//    scanf("%d",&c);
+//    printf("%d\n%d\n%d",a,b,c);
+//    Zad1.2.7
+//    int a;
+//    printf("Podaj liczbe\n");
+//    scanf("%d",&a);
+//    a++;
+//    printf("%d",a);
+//    Zad1.2.8
+//    printf("Podaj trzy liczby\n");
+//    int a,b,c;
+//    scanf("%d",&a);
+//    scanf("%d",&b);
+//    scanf("%d",&c);
+//    int d = (a+b+c)/3;
+//    printf("Ich œrednia to: %d",d);
+//    Zad1.2.9
+//    printf("Podaj liczbe nieujemn¹\n");
+//    unsigned int a;
+//    scanf("%d",&a);
+//    printf("Pierwiastek: %f",sqrt(a));
+//    Zad1.2.10
+//    printf("Podaj liczbê wymiern¹\n");
+//    float a;
+//    scanf("%f",&a);
+//    printf("abs: %d",abs(a));
+//    Zad1.2.11
+//    printf("Podaj liczbê wymiern¹");
+//    float a;
+//    scanf("%f",&a);
+//    printf("Do dwoch: %.2f",a);
+//    Zad1.2.12
+//    printf("Podaj liczbe wymierna\n");
+//    float a;
+//    scanf("%f",&a);
+//    printf("%e",a);
+//    Zad1.3.1
+//    printf("Podaj liczbe calkowita\n");
+//    int a;
+//    scanf("%d",&a);
+//    if(a < 0){
+//        a *= -1;
+//    }
+//    printf("abs: %d",a);
+//    Zad1.3.2
+//    printf("Podaj dwie liczby calkowite\n");
+//    int a,b;
+//    scanf("%d",&a);
+//    scanf("%d",&b);
+//    if(a > b){
+//        printf("Wieksza %d",a);
+//    }
+//    else if(b > a){
+//        printf("Wieksza %d",b);
+//    }
+//    else{
+//        printf("%d",a);
+//    }
+//    Zad1.3.3
+//    printf("Podaj trzy liczby calkowite\n");
+//    int a,b,c;
+//    scanf("%d",&a);
+//    scanf("%d",&b);
+//    scanf("%d",&c);
+//    if(a > b && a > c){
+//        printf("najwieksza: %d",a);
+//    }
+//    else if(b > a && b > c){
+//        printf("najwieksza: %d",b);
+//    }
+//    else if(c > a && c > b){
+//        printf("najwieksza: %d",c);
+//    }
+//
+//    else if (a == b && b == c){
+//        printf("liczby sa rowne");
+//    }
+//    Zad1.3.4
+//    printf("Podaj dwie liczby calkowite\n");
+//    int a,b;
+//    scanf("%d",&a);
+//    scanf("%d",&b);
+//    if(abs(a) > abs(b)){
+//        printf("wiekszy abs: %d",a);
+//    }
+//    else if(abs(b) > abs(a)){
+//        printf("wiekszy abs: %d",b);
+//    }
+//    Zad1.3.5
+//    printf("Podaj sposob obliczenia pola trojkata:\n1-boki\n2-podstawa i wysokosc\n");
+//    int s;
+//    float w;
+//    scanf("%d",&s);
+//    if(s == 1){
+//        printf("Podaj boki trojkata\n");
+//        float a,b,c;
+//        scanf("%f",&a);
+//        scanf("%f",&b);
+//        scanf("%f",&c);
+//        float p = (a+b+c)/2;
+//
+//        w = sqrt(p*(p-a)*(p-b)*(p-c));
+//        printf("Pole to %f cm^2,",w);
+//    }
+//    else if(s == 2){
+//        printf("Podaj podstawe i wysokosc\n");
+//        float a,b;
+//        scanf("%f",&a);
+//        scanf("%f",&b);
+//        w = (a*b)/2;
+//        printf("Pole to %f cm^2",w);
+//    }
+//    else{
+//        printf("B³¹d");
+//    }
+    printf("Podaj wspolczynniki rownania liniowego\n");
+
+    return 0;
+}
